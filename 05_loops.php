8dbe60d8b09c9dac714bc09d4c0b6d844185da43
LuYujin9 <wuwujane@hotmail.com>
2023-05-15 11:45:09 +0200

    tied up

--- 05_loops.php
+++ 05_loops.php
@@ -11,9 +11,6 @@
   }
 */
 
-for ($x = 0; $x <= 10; $x++) {
-  echo "Number: $x <br>";
-}
 
 /* ------------ While Loop ------------ */
 
@@ -24,12 +21,6 @@ for ($x = 0; $x <= 10; $x++) {
   }
 */
 
-$x = 1;
-
-while ($x <= 5) {
-  echo "Number: $x <br>";
-  $x++;
-}
 
 /* ---------- Do While Loop --------- */
 
@@ -42,10 +33,7 @@ while ($x <= 5) {
 do...while loop will always execute the block of code once, even if the condition is false.
 */
 
-do {
-  echo "Number: $x <br>";
-  $x++;
-} while ($x <= 5);
+
 
 /* ---------- Foreach Loop ---------- */
 
@@ -55,32 +43,3 @@ do {
   // code to be executed
   }
 */
-
-// Loop through an array
-
-$numbers = [1, 2, 3, 4, 5];
-
-foreach ($numbers as $x) {
-  echo "Number: $x <br>";
-}
-
-// Use the indexes within the loop
-
-$posts = ['Post One', 'Post Two', 'Post Three'];
-
-foreach ($posts as $index => $post) {
-  echo "${index} - ${post} <br>";
-}
-
-// Use the keys within the loop for an associative array
-
-$person = [
-  'first_name' => 'Brad',
-  'last_name' => 'Traversy',
-  'email' => 'brad@gmail.com',
-];
-
-// Get Keys
-foreach ($person as $key => $val) {
-  echo "${key} - ${val} <br>";
-}
