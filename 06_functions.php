36188643a96909774444f7da951f8b6e6561ea4e
LuYujin9 <wuwujane@hotmail.com>
2023-05-16 12:44:56 +0200

    6

--- 06_functions.php
+++ 06_functions.php
@@ -1,14 +1,45 @@
 <?php
 /* ------------ Functions ----------- */
+$y = 'Email is';
 
-/*
-  Functions are reusable blocks of code that we can call to perform a specific task.
-  We can pass values into functions to change their behavior. Functions have their own local scope as opposed to global scope
+function registerUser($email)
+{
+  // echo $y; can't directly access 
+  global $y;
+  echo $y;
+  $x = 10; // only in function
+  echo $email . 'is registered';
+}
+
+echo $x;
+registerUser('123@hotmail.com');
+echo '<br>';
+
+function sum($n1, $n2)
+{
+  return $n1 + $n2;
+}
+
+$number = sum(5, 5);
+echo $number;
+echo '<br>';
+
+function sumTwo($n1 = 4, $n2 = 5) //set default value
+{
+  return $n1 + $n2;
+}
+$number = sumTwo();
+echo $number;
+$number = sumTwo(5, 5);
+echo $number;
+
+/* ------------ Arrow functions ----------- */
+$subtract = function ($n1, $n2) {
+  return $n1 - $n2;
+};
+
+echo $subtract(1, 2);
 
 
-/*
-** Function Syntax
-  function functionName($arg1, $arg2, ...) {
-    // code to be executed
-  }
-*/
+$multiply = fn ($n1, $n2) => $n1 - $n2;
+echo $multiply(1, 2);
